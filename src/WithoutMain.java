d9b91c8cc3872beb45ada7fc9ae327c34f890969
Musharaf Basha <Musharaf Basha@LAPTOP-0CIE9VDU>
2025-06-06 06:54:41 +0530

    Main

--- src/WithoutMain.java
+++ src/WithoutMain.java
@@ -1,8 +1,10 @@
 class WithoutMain {
-	Static{
-		System.out.println("Hello World!");
-System.exit(0);
-	}
-	public static void main(String args[]) {
-	}
+    static {
+        System.out.println("Hello World!");
+        System.exit(0);
+    }
+
+    public static void main(String[] args) {
+        // This will never be called due to System.exit(0) in static block
+    }
 }
